3a299f9244d35153edea9d508bf4d826b78c2d8a
RileyBoroff <102422258+RileyBoroff@users.noreply.github.com>
2023-01-24 13:11:14 -0500

    New folder to store list
    To-DoTUI now stores all list under one folder to keep things organized.

--- TodoTUI.sh
+++ TodoTUI.sh
@@ -4,14 +4,14 @@ new_list () {
     #creates a new to-do list and adds the file name to the first line of the file
     echo "enter a name for the new list"
     read list
-    touch $list.txt
-    echo $list >> $list.txt
+    touch list/$list.txt
+    echo $list >> list/$list.txt
 }
 
 view_names () {
     #shows the names of all the to-do list without the file extention
     echo "all todo list"
-    ls *.txt | sed -e 's/\.txt$//'   
+    ls list/*.txt | sed -e 's/\.txt$//'   
 }
 clear
 echo 'Welcom to to-doTUI'
@@ -25,28 +25,28 @@ read -e -p "To-DoTUI>" options
         view_names
         echo "enter a to-do list"
         read list
-        if [ -w $list.txt ];
+        if [ -w list/$list.txt ];
         #checks if a writable file with the given name exist 
         then
             echo "enter a item to add to the list"
             read item
             echo "add a due date"
             read date
-            echo $item $date >> $list.txt          
+            echo $item $date >> list/$list.txt          
         else
             touch $list.txt
-            echo $list >> $list.txt
+            echo $list >> list/$list.txt
             echo "enter a item to add to the list"
             read item
             echo "add a due date"
             read date
-            echo $item $date >> $list.txt
+            echo $item $date >> list/$list.txt
         fi;;
 
         "view a")
         #view all to-do list
         echo "all todo list"
-        cat *.txt;;
+        cat list/*.txt;;
  
         "new l")
         #add new to-do list
@@ -57,14 +57,14 @@ read -e -p "To-DoTUI>" options
         view_names       
         echo "enter the name of the list to be deleted"
         read list
-        rm $list.txt;;
+        rm list/$list.txt;;
 
         "view")
         #view specific to-do list
         view_names
         echo "enter the name of the list to view"
         read list
-        less $list.txt;;
+        less lsit/$list.txt;;
 
         "view n")
         #view all to-do list names
